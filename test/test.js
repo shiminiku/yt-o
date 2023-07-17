e92c55295e8098faaddba07836447a85b03ca8e6
owatu <34957270+owatu@users.noreply.github.com>
2023-07-18 02:55:32 +0900

    change, test
    change, not test, fail

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import { getPlayerResponse, getVideoURL } from "../dist/index"
+import { getPlayerResponse, getSCVideoURL, getVideoURL } from "../dist/index"
 import got from "got"
 
 const VIDEO_ID = "dQw4w9WgXcQ"
@@ -9,13 +9,28 @@ test("getPlayerResponse", async () => {
 })
 
 test("getURL", async () => {
+  const { playerResponse, basejsURL } = await getPlayerResponse("jNQXAC9IVRw")
+  const formats = playerResponse?.streamingData?.formats
+  const url = formats.at(-1)?.url
+
+  expect(formats && url).toBeTruthy()
+
+  const videoURL = await getVideoURL(url, basejsURL)
+  expect(videoURL?.search(/^https:\/\/[^.]*\.googlevideo\.com/) > -1).toBe(true)
+
+  const response = await got(videoURL, { method: "HEAD" })
+  console.log(response.statusCode)
+  expect(response.statusCode).toBe(200)
+})
+
+test("getSCURL", async () => {
   const { playerResponse, basejsURL } = await getPlayerResponse(VIDEO_ID)
   const formats = playerResponse?.streamingData?.formats
   const signatureCipher = formats.at(-1)?.signatureCipher
 
   expect(formats && signatureCipher).toBeTruthy()
 
-  const videoURL = await getVideoURL(signatureCipher, basejsURL)
+  const videoURL = await getSCVideoURL(signatureCipher, basejsURL)
   expect(videoURL?.search(/^https:\/\/[^.]*\.googlevideo\.com/) > -1).toBe(true)
 
   const response = await got(videoURL, { method: "HEAD" })
